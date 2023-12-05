090189bd59eb02e57952a4b7b4b1d7d10847725a
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2023-12-05 11:45:21 +0900

    타이틀 수정

--- src/components/views/main/main.js
+++ src/components/views/main/main.js
@@ -14,7 +14,7 @@ function Main() {
     }
     return (
         <div className="root_container">
-            <h1 className="title">대충 급조 해서 만든 가계부</h1>
+            <h1 className="title">타이틀</h1>
             <div className="input_container">
                 <input className="id" type="text" placeholder="ID" maxLength={id_max_length} required /><br />
                 <input className="password" type="text" placeholder="Password" maxLength={pw_max_length} /><br />
