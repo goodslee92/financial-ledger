01104cb7a851fc548ca957d7eac2e07ff16d9202
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2023-12-18 15:09:54 +0900

    fix intent

--- src/utils/numberUtils.js
+++ src/utils/numberUtils.js
@@ -2,11 +2,10 @@ export const enteredOnlyNumber = (val) => {
     return val.replace(/[^0-9]/g, "");
   };
   
-  export const addComma = (num) => {
-    return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  };
-  
-  export const deleteComma = (str) => {
-    return str.replace(/,/g, "");
-  };
-  
+export const addComma = (num) => {
+  return num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const deleteComma = (str) => {
+  return str.replace(/,/g, "");
+};
