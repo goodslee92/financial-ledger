dda1a7fac87be0fff8747c493517fe8105e900df
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2024-01-30 12:40:50 +0900

    네비게이션 선택 탭 밑줄 표시

--- src/components/common/nav/nav.js
+++ src/components/common/nav/nav.js
@@ -1,17 +1,61 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
 
 function Nav() {
+    const buttonList = [
+        { text: "내역 추가", path: "/Home" },
+        { text: "달력", path: "/Calendar" },
+        { text: "일일", path: "/Daily" },
+        { text: "통계", path: "/Statistics" },
+      ];
+    const [selected, setSelected] = useState();
+    const buttonHandler = (text) => {
+        setSelected(text);
+    };
+    const classes = useStyles();
     return (
         <div>
             <div className="navbar">
-                <Link className="navbarMenu" to={'/Home'}>내역 추가</Link>
+                {
+                    buttonList.map((item, index) => {
+                        return (
+                            <Link className={item.text === selected ? classes.underLine : classes.underLine_none}
+                                to={item.path}
+                                key={index}
+                                onClick={() => {
+                                    buttonHandler(item.text);
+                                }}
+                            >
+                                { item.text }
+                            </Link>
+                        )
+                    })
+                }
+                {/* <Link className="navbarMenu" to={'/Home'}>내역 추가</Link>
                 <Link className="navbarMenu" to={'/Calendar'}>달력</Link>
                 <Link className="navbarMenu" to={'/Daily'}>일일</Link>
-                <Link className="navbarMenu" to={'/Statistics'}>통계</Link>
+                <Link className="navbarMenu" to={'/Statistics'}>통계</Link> */}
             </div>
         </div>
     );
 }
+const useStyles = makeStyles({
+    // 밑줄 비표시
+    underLine_none: {
+        color: "black",
+        width: "20%",
+        textDecoration: "none",
+      },
+    // 버튼 선택시 빨간 밑줄 표시
+    underLine: {
+        color: "black",
+        width: "20%",
+        textDecoration: "underline",
+        textDecorationColor: "#ff5b5b",
+        textDecorationThickness: "5px",
+        fontWeight: "bold",
+    },
+});
 
 export default Nav;
