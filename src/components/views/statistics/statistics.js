aad13947d5ec56f5f6fab0f51a3e547f7dd1ad20
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2024-02-13 17:30:14 +0900

    분류 옵션 추가

--- src/components/views/statistics/statistics.js
+++ src/components/views/statistics/statistics.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Select from "react-select"
+import Select from "react-select";
 import './statistics.scss';
 import HeaderAmount from '../../common/header/header_amount';
 import { addComma } from '../../../utils/numberUtils';
-import Nav from '../../common/nav/nav'
+import Nav from '../../common/nav/nav';
 import HeaderTitle from "../../common/header/header_title";
-import axios from 'axios'
-import { url } from '../../common/api'
+import axios from 'axios';
+import { url } from '../../common/api';
 import RoundBtn from "../../common/roundBtn/roundBtn";
 
 const Dropdown = () => {
@@ -14,41 +14,46 @@ const Dropdown = () => {
         { value: "주간", label: "주간" },
         { value: "월간", label: "월간" },
         { value: "연간", label: "연간" }
-    ]
+    ];
     const secondOptions = [
+        { value: "저축", label: "저축" },
         { value: "의료비", label: "의료비" },
         { value: "교통비", label: "교통비" },
         { value: "외식비", label: "외식비" },
         { value: "문화비", label: "문화비" },
         { value: "보험료", label: "보험료" },
         { value: "공과금", label: "공과금" },
-        { value: "통신비", label: "통신비" },
-    ]
-    const [totalSum, setTotalSum ] = useState(0)
-    const [totalIncome, setTotalIncome] = useState(0)
-    const [totalOutcome, setTotalOutcome] = useState(0)
-    const total = { sum: 0, income: 0, outcome: 0 }
+        { value: "통신비", label: "통신비" }
+    ];
+    const [totalSum, setTotalSum ] = useState(0);
+    const [totalIncome, setTotalIncome] = useState(0);
+    const [totalOutcome, setTotalOutcome] = useState(0);
+    const total = { sum: 0, income: 0, outcome: 0 };
     const data = {
         id: window.sessionStorage.getItem('loginUserId'),
         year: new Date().getFullYear()
-    }
-    const [financialList, setFinancialList] = useState()
+    };
+    const [financialList, setFinancialList] = useState();
     const [selectedPeriod, setSelectedPeriod] = useState('주간');
-    const handleOnChange = (e) => {
+    const [selectedExpense, setSelectedExpense] = useState('');
+    const handlePeriodOnChange = (e) => {
         setSelectedPeriod(e.value);
-    }
+    };
+    const handleExpenseOnChange = (e) => {
+        setSelectedExpense(e.value);
+    };
     // 헤더 금액 부분 계산
     const calcTotalIncome = () => {
         financialList && financialList.map((content, index) => {
-            total.income += content.TOTAL_INCOME 
-            total.outcome += content.TOTAL_OUTCOME
-        })
-        total.sum = total.income - total.outcome
-        setTotalIncome(total.income)
-        setTotalOutcome(total.outcome)
-        setTotalSum(total.sum)
-        console.log('total.income : ' + total.income + ', total.outcome : ' + total.outcome + ', total.sum : ' + total.sum)
-    }
+            total.income += content.TOTAL_INCOME;
+            total.outcome += content.TOTAL_OUTCOME;
+        });
+        total.sum = total.income - total.outcome;
+        setTotalIncome(total.income);
+        setTotalOutcome(total.outcome);
+        setTotalSum(total.sum);
+        console.log('total.income : ' + total.income + ', total.outcome : ' + total.outcome + ', total.sum : ' + total.sum);
+    };
     // 선택값(주간/월간/연간)에 따라 조회 쿼리 분기처리
     const switchPeriod = () => {
         switch (selectedPeriod) {
@@ -61,10 +66,10 @@ const Dropdown = () => {
                         setFinancialList(res.data)
                     }).catch(err => {
                         console.log(err)
-                    })
-                }
-                fetchMonthlyData()
-                break
+                    });
+                };
+                fetchMonthlyData();
+                break;
             case '연간' :
                 const fetchYearlyData = async () => {
                     await axios.post(url + '/api/selectYearlyItem', data)
@@ -73,10 +78,10 @@ const Dropdown = () => {
                         setFinancialList(res.data)
                     }).catch(err => {
                         console.log(err)
-                    })
-                }
-                fetchYearlyData()
-                break
+                    });
+                };
+                fetchYearlyData();
+                break;
             default :
                 // 주간
                 const fetchData = async () => {
@@ -86,22 +91,23 @@ const Dropdown = () => {
                         setFinancialList(res.data)
                     }).catch(err => {
                         console.log(err)
-                    })
-                }
-                fetchData()
-                break
+                    });
+                };
+                fetchData();
+                break;
         }
-    }
+    };
     useEffect(() => {
-        console.log('selectedPeriod is changed, selectedPeriod : ' + selectedPeriod)
+        console.log('selectedPeriod is changed, selectedPeriod : ' + selectedPeriod);
         // 선택값(주간/월간/연간) 변동시 해당 기간의 조회 쿼리 분기처리
-        switchPeriod()
-    }, [selectedPeriod])
+        switchPeriod();
+    }, [selectedPeriod]);
 
     useEffect(() => {
         // 기간(주간/월간/연간)에 따른 데이터 변경시 헤더(금액부분) 새로 계산
-        calcTotalIncome()
-    }, [financialList])
+        calcTotalIncome();
+    }, [financialList]);
+
     return (
         <div className="statistics_root_container">
             <HeaderTitle />
@@ -109,8 +115,13 @@ const Dropdown = () => {
             <div className="statistics_container">
                 <HeaderAmount income={totalIncome.toString()} outcome={totalOutcome.toString()} sum={totalSum.toString()}/>
                 <div className="dropDown_container">
-                    <div className="select_div">
-                        <Select defaultValue={firstOptions[0]} options={firstOptions} className="select" onChange={handleOnChange} />
+                    <div className="first_option_container">
+                        <p>기간</p>
+                        <Select defaultValue={firstOptions[0]} options={firstOptions} className="select" onChange={handlePeriodOnChange} />
+                    </div>
+                    <div className="second_option_container">
+                        <p>분류</p>
+                        <Select options={secondOptions} className="select" onChange={handleExpenseOnChange} />
                     </div>
                 </div>
                 <div className="statistics_content_container">
