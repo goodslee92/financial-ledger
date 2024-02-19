f8c7209655211dcdf3367bbc130d0db33689e3f5
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2024-02-19 13:53:25 +0900

    배경 이미지  추가

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import "./styles/button.scss";
 import './components/views/newItem/newItem.scss';
 import './components/common/nav/nav.scss'
 import './components/common/header/header.scss';
+import backgoundImg from './image/background.png';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Index />}/>
-          <Route path="/Calendar" element={<Calendar />}/>
-          <Route path="/Monthly" element={<Monthly />}/>
-          <Route path="/Statistics" element={<Statistics />}/>
-          <Route path="/SignUp" element={<SignUp />} />
-        </Routes>
+        <div className='background_img_container' style={{ position : 'absolute', zindex : '0', width : '100vw', height : '100vh', backgroundImage : 'url(' + backgoundImg + ')', backgroundRepeat : 'no-repeat', backgroundPosition : 'center', backgroundSize : '100% 100%', opacity : '0.2'}}>
+        </div>
+          <Routes>
+            <Route path="/" element={<Index />}/>
+            <Route path="/Calendar" element={<Calendar />}/>
+            <Route path="/Monthly" element={<Monthly />}/>
+            <Route path="/Statistics" element={<Statistics />}/>
+            <Route path="/SignUp" element={<SignUp />} />
+          </Routes>
       </div>
     </BrowserRouter>
   );
