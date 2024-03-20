8e8386f19121dc713c995783eee258225584e541
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2024-03-20 17:35:55 +0900

    앱 종료버튼 추가

--- src/components/common/header/header_title.js
+++ src/components/common/header/header_title.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
+import { BackHandler } from 'react-native';
 import { IoMenu } from "react-icons/io5";
 import Modal from 'react-modal';
 import Menu from '../menu/menu';
+import { RiLogoutBoxLine } from "react-icons/ri";
+
 const HeaderTitle = () => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const onCloseHandler = () => {
-        setModalIsOpen(false)
+        setModalIsOpen(false);
+    }
+    const exitOnClickHandler = () => {
+        if (window.confirm('마이머니북을 종료하시겠습니까?')) {
+            BackHandler.exitApp();
+        } else {
+            console.log('Cancel Pressed');
+        }
     }
     return (
         <div className="header_title_container">
@@ -17,6 +27,9 @@ const HeaderTitle = () => {
             <Modal className='menuModal' isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} ariaHideApp={false}>
                 <Menu isOpen={modalIsOpen} onClose={onCloseHandler} />
             </Modal>
+            <div className='exit_button'>
+                <button className='exit_app_btn' onClick={exitOnClickHandler}><RiLogoutBoxLine /></button>
+            </div>
         </div>
     )
 }
