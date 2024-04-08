991cfa397bd2f33e51293ba1c432fa19321bac24
G07\lee.jaehwa <lee.jaehwa@fujitsu.com>
2024-04-08 13:03:09 +0900

    api 서버 클라우드 타입으로 롤백

--- src/components/common/api.js
+++ src/components/common/api.js
@@ -1,3 +1,7 @@
+// 클라우드타입
 export const url = 'https://port-0-financial-ledger-backend-jvpb2mloiaxyw7.sel5.cloudtype.app';
+// 오라클 클라우드 VM
+// export const url = 'http://144.24.75.220:3306';    
+// 로컬
 // export const url = 'http://localhost:3001';
 
